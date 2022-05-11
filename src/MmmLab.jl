a51d5fb2fd94149186a4f8cd9acf149e4319dae2
Hok Chio (Mark) Lai <marklhc@gmail.com>
2022-05-10 17:31:47 -0700

    Add export statement

--- src/MmmLab.jl
+++ src/MmmLab.jl
@@ -1,12 +1,16 @@
 module MmmLab
 
+export greet, greet3
+
 import Random
 import DataFrames
 import JSON
 
-greet() = print("Hi MMM Lab!")
-function greet2()
+greet() = print("Hi MMM Lab!!!!")
+function greet3()
     return "return  this"
 end
+
+#export greet, greet2
 
 end # module
